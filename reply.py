1278881fd4b55d1aeeb2af8d355996e4c1e4ea19
nk1976 <you@example.com>
2023-11-14 14:59:39 -0500

    First commit

---
+++ reply.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome(executable_path='/path/to/chromedriver')
+driver.get('https://messages.google.com/web')
+
+# Logic to log in goes here
+username = "your_username"
+password = "your_password"
+
+# Set up the WebDriver
+driver = webdriver.Chrome(executable_path='/path/to/chromedriver')
+driver.get('https://messages.google.com/web')
+
+# Find and fill the username field
+username_field = driver.find_element_by_id('identifierId')
+username_field.send_keys(username)
+username_field.send_keys(Keys.RETURN)
+
+# Wait for the next page to load and find the password field
+# You might need to add an explicit wait here
+password_field = driver.find_element_by_name('password')
+password_field.send_keys(password)
+password_field.send_keys(Keys.RETURN)
+
+# Now, monitor for new messages
+while True:
+    try:
+        # Wait for the presence of a new message element, with a timeout
+        new_message = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, 'identifier_for_new_message'))
+        )
+
+        # Logic to respond to the message
+        # ...
+
+    except TimeoutException:
+        # Handle the case where no new message appears within the timeout
+        pass
